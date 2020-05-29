5e2aa70c888f30161111bb09ff306d16e2c21d02
Lucas Augusto <lucasamgs@gmail.com>
2020-05-29 20:18:05 -0300

    Leitura dos dados

--- src/Prova.java
+++ src/Prova.java
@@ -1,9 +1,35 @@
-
+import java.util.Locale;
+import java.util.Scanner;
 public class Prova {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		int horas,thoras,opçmen;
+		double vph,custo,caro1,caro2;
+		char opçao;
+		
+		thoras=0;
+		custo=0;		
+		
+		System.out.print("Nome:");
+		nome = sc.next();
+		System.out.print("Horas trabalhadas:");
+		horas = sc.nextInt();
+		System.out.print("Valor por hora:");
+		vph = sc.nextDouble();
+		thoras=thoras + horas;
+		custo = custo + (vph*horas);
+		System.out.print("digitar outro(S/N)?");
+		opçao = sc.next().charAt(0);
+		
+		
+
 
 }
+}
+
